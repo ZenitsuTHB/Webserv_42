44b97e631392735f943d68ed3d947166b3eba4e7
Maximiliano Velazquez <mvelazqu@cbr11s1.42barcelona.com>
2025-03-05 19:26:06 +0100

    Base Socket (base)

---
+++ includes/BaseSocket.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         :::      ::::::::  */
+/*  BaseSocket.hpp                                       :+:      :+:    :+:  */
+/*                                                     +:+ +:+         +:+    */
+/*  By: mvelazqu <mvelazqu@student.42barcelona.c     +#+  +:+       +#+       */
+/*                                                 +#+#+#+#+#+   +#+          */
+/*  Created: 2025/03/05 18:26:19 by mvelazqu            #+#    #+#            */
+/*  Updated: 2025/03/05 19:25:20 by mvelazqu           ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASESOCKET_HPP
+# define BASESOCKET_HPP
+
+# include <sys/socket.h>
+# include <netinet/in.h>
+
+class	BaseSocket
+{
+	public:
+		~BaseSocket( void );
+		BaseSocket( void );
+		BaseSocket( int fd );
+		BaseSocket( int domain, int type, int protocol );
+		BaseSocket( BaseSocket const &obj );
+
+		void	create( int domain, int type, int protocol );
+		void	bind( struct sockaddr_in address );
+		void	bind( int ip, int port );
+		void	close( void );
+
+		void	setSockFd( int fd );
+		void	setAddress( struct sockaddr_in address, socklen_t len);
+
+		int	getSockFd( void ) const { return (_sockFd) };
+		int	getPort( void ) const { return (htons(_address.sin_port)) }
+		int	getIp( void ) const { return (htons(_address.sin_addr.s_addr)) }
+
+		BaseSocket	& operator = ( BaseSocket const &obj );
+
+	private:
+		int					_sockFd;
+		int					_domain;
+		int					_type;
+		int					_protocol;
+		struct sockaddr_in	_address;
+		socklen_t			_addrLen;
+};
+
+#endif
---
+++ sources/BaseSocket.cpp
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         :::      ::::::::  */
+/*  BaseSocket.cpp                                       :+:      :+:    :+:  */
+/*                                                     +:+ +:+         +:+    */
+/*  By: mvelazqu <mvelazqu@student.42barcelona.c     +#+  +:+       +#+       */
+/*                                                 +#+#+#+#+#+   +#+          */
+/*  Created: 2025/03/05 18:26:18 by mvelazqu            #+#    #+#            */
+/*  Updated: 2025/03/05 19:25:21 by mvelazqu           ###   ########.fr      */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BaseSocket.hpp"
+#include <iostream>
+#include <stdio.h>
+
+BaseSocket::~BaseSocket(void)
+{
+	return ;
+}
+
+BaseSocket::BaseSocket(void): _sockFd(-1), _domain(0), _type(0), _protocol(0),
+	_address(sizeof(_address))
+{
+	_address.sin_family = 0;
+	_address.sin_port = 0;
+	_address.sin_addr.s_addr = 0;
+	return ;
+}
+
+BaseSocket::BaseSocket(int fd): _sockFd(fd), _domain(0), _type(0), _protocol(0),
+	_address(sizeof(_address))
+{
+	_address.sin_family = 0;
+	_address.sin_port = 0;
+	_address.sin_addr.s_addr = 0;
+}
+
+BaseSocket::BaseSocket(int domain, int type, int protocol):
+	_address(sizeof(_address))
+{
+	_address.sin_family = domain;
+	_address.sin_port = 0;
+	_address.sin_addr.s_addr = 0;
+	create(domain, type, protocol);
+	return ;
+}
+
+BaseSocket::BaseSocket(BaseSocket const &obj)
+{
+	*this = obj;
+}
+
+void	BaseSocket::setSockFd(int fd)
+{
+	if (_sockFd != -1)
+		::close(_sockFd);
+	_sockFd = fd;
+}
+
+void	BaseSocket::setAddress( struct sockaddr_in address, socklen_t len)
+{
+	if (len > _addrLen)
+		std::cerr << "<BaseSocket> Setted address lenght too long" << std::endl;
+	_address = len;
+	_address = address;
+}
+
+void	BaseSocket::create(int domain, int type, int protocol)
+{
+	if (_sockFd != -1)
+		return ;
+	_sockFd = socket(domain, type, protocol);
+	if (_sockFd == -1)
+		return (perror("<BaseSocket> Error sucking"));
+	_protocol = protocol;
+	_domain = domain;
+	_type = type;
+	_address.sin_family = domain;
+}
+
+void	BaseSocket::bind(struct sockaddr_in address)
+{
+	_address = address;
+	if (::bind(_sockFd, (struct sockaddr *)&_address, sizeof(_address)) == -1)
+		return (perror("<BaseSocket> Error binding"), close());
+}
+
+void	BaseSocket::bind(int ip, int port)
+{
+	_address.sin_port = htons(port);
+	_address.sin_addr.s_addr = htonl(ip);
+	if (::bind(_sockFd, (struct sockaddr *)&_address, sizeof(_address)) == -1)
+		return (perror("<BaseSocket> Error binding"), close());
+}
+
+void	BaseSocket::close(void)
+{
+	if (_sockFd == -1)
+		return ;
+	::close(_sockFd);
+	_sockFd = -1;
+}
+
+BaseSocket	&BaseSocket:: operator = (BaseSocket const &obj)
+{
+	if (this != &obj || _sockFd != obj._sockFd)
+	{
+		_sockFd = obj._sockFd;
+		_domain = obj._domain;
+		_type = obj._type;
+		_protocol = obj._protocol;
+		_address = obj._address;
+		_addrLen = obj._addrLen;
+	}
+	return (*this);
+}
