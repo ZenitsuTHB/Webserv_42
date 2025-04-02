9040b1201f1745da24583a4c6a5687b1ff7e7f0c
ZenitsuTHB <starxyz12@gmail.com>
2025-04-02 18:44:43 +0200

    Emergency push

--- sources/Server2.cpp
+++ sources/Server2.cpp
@@ -6,7 +6,7 @@
 /*   By: avolcy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 16:55:48 by avolcy            #+#    #+#             */
-/*   Updated: 2025/04/01 19:15:04 by avolcy           ###   ########.fr       */
+/*   Updated: 2025/04/02 18:43:55 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,8 +61,8 @@ Server::Server(int domain, int type, int protocol):
         if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1)
         {
                 _socket.close();
-                perror("<Server> Error in opt");
-                return ;
+               return ( perror("<Server> Error in opt"));
+                
         }
 
 	_socket.setNonBlocking( fd, true );
@@ -70,36 +70,37 @@ Server::Server(int domain, int type, int protocol):
         _clientfd = -1;
         _epoll_fd = epoll_create1( 0 );
         if ( _epoll_fd == -1 )
-        {
-                perror("<SERVER> Failed to create epoll instance");
-                return ;
-        }
-
+		return (perror("<SERVER> Failed to create epoll instance"));
         return ;
 }
-
+../includes/Server.hpp
 Server::Server(Server const &obj):
         _socket(obj._socket)
 {
         *this = obj;
 }
 
-
-
-void    Server::start(int ip, int port, int backlog)
+void    Server::close( int fd )
 {
-        _socket.bind(ip, port);
-        _socket.listen(backlog);
+    if ( clientsMap.find( fd ) == clientsMap.end() ) return;
+    clientsMap[fd].close();
+    epoll_ctl( _epoll_fd, EPOLL_CTL_DEL, fd, NULL );
+    clientsMap.erase( fd );
+}
+
+void    Server::start( int ip, int port, int backlog )
+{
+        _socket.bind( ip, port );
+        _socket.listen( backlog );
         _socket.setNonBlocking( true );
 
         struct  epoll_event event;
         event.events = EPOLLIN | EPOLLET;
         event.data.fd = _socket.getSockFd();
         if( epoll_ctl( _epoll_fd, EPOLL_CTL_ADD, _socket.getSockFd(), &event) == -1 )
-                throw std::runtime_error("<SERVER> Failed to add socket to epoll: " +
+                throw std::runtime_error( "<SERVER> Failed to add socket to epoll: " +
                                 std::string(strerror(errno)));
         _running = true;
-
         std::cout << "<SERVER> started on ip : [ " << ip << " ] and port : [ " << port << " ]\n"
 }
 
@@ -108,7 +109,7 @@ void	Server::acceptNewConnection( void )
     while (true)
     {
         BaseSocket clientSocket = _socket.accept();
-        if (clientSocket.getSockFd() == -1)
+        if ( clientSocket.getSockFd() == -1)
         {
                 if ( errno == EAGAIN || errno == EWOULDBLOCK )
                         break;
@@ -116,7 +117,7 @@ void	Server::acceptNewConnection( void )
                         return( perror( "<Server> Error accepting client" ));
         }
 
-        _clientList[clientSocket.getSockFd()] = clientSocket;
+        clientsMap[clientSocket.getSockFd()] = clientSocket;
 	clientSocket.setNonBlocking( true );
 
         struct epoll_event event;
@@ -124,14 +125,36 @@ void	Server::acceptNewConnection( void )
         event.events = EPOLLIN | EPOLLRDHUP | EPOLLET | EPOLLHUP;
         event.data.fd = clientSocket.getSockFd();
 
-        if ( epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, clientSocket.getSockFd(), &event) == -1 )
+        if ( epoll_ctl( _epoll_fd, EPOLL_CTL_ADD, clientSocket.getSockFd(), &event ) == -1 )
         {
-            perror("<Server> Failed to add client to epoll");
-            clientSocket.close();
-            _clientList.erase(clientSocket.getSockFd());
+            perror( "<Server> Failed to add client to epoll" );
+            clientSocket.close( clientSocket.getSockFd() );
         }
     }
-    return 0;
+    return ;
+}
+
+void	Seerver::run( void )
+{
+	while ( _running )
+	{
+		int eventCounter = epoll_wait( _epoll_fd, _events, MAX_EVENTS, -1);
+		if ( eventCounter == -1 )
+		{
+			if( errno == EINTR ) continue;
+			throw std::runtime_error( "<SERVER> epoll_wait failed " + std::string(strerror( errno )));
+		}
+		
+		for ( int i = 0; i < eventCounter; i++)
+		{
+			int clientFd = _events[i].data.fd;//new client
+			if ( eventFd == _socket.getSockFd() )
+                                acceptNewConnection();
+                        else
+                                handleClientEvent( clientFd, _events );//manage connected client
+		}
+
+	}
 }
 
 int	main() 
